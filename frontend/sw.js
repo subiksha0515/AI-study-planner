3fe9028490c1bd19ad196e97122ef69cc343dffe
selvaganesh19 <selvaganesh.al22@bitsathy.ac.in>
2025-11-16 21:49:09 +0530

    Added service worker file
    Co-authored-by: Friend Name <kvijayalakshmi3379@gmail.com>

---
+++ frontend/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'sleep-disorder-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdn.jsdelivr.net/npm/@gradio/client/dist/index.min.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
